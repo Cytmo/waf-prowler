efd0c3d58941357b240f5a55decacc573ebf783b
cytmo <cytmo@qq.com>
2024-08-27 18:00:41 +0800

    feat. add another mutant method

--- utils/prowler_mutant.py
+++ utils/prowler_mutant.py
@@ -10,9 +10,14 @@ def mutant_methods_modify_content_type(headers,url,method,data,files):
         headers['Content-Type'] += ';application/json'
     return headers,url,method,data,files
 
+def mutant_methods_fake_content_type(headers,url,method,data,files):
+    logger.info(TAG + "==>mutant_methods_fake_content_type")
+    logger.debug(TAG + "==>headers: " + str(headers))
+    if 'Content-Type' in headers:
+        headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    return headers,url,method,data,files
 
-
-mutant_methods = [mutant_methods_modify_content_type]
+mutant_methods = [mutant_methods_modify_content_type,mutant_methods_fake_content_type]
 
 
 def prowler_begin_to_mutant_payloads(headers, url, method, data,files):
