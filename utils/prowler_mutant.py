d4349ff9a53d30c6620d2b3c4b0c35f10612045f
stcirclear <1433481694@qq.com>
2024-08-27 23:41:47 +0800

    add change_content_type

--- utils/prowler_mutant.py
+++ utils/prowler_mutant.py
@@ -1,32 +1,54 @@
 from utils.logUtils import LoggerSingleton
+from utils.dictUtils import content_types
+
 logger = LoggerSingleton().get_logger()
 TAG = "prowler_mutant.py: "
 
 
-def mutant_methods_modify_content_type(headers,url,method,data,files):
+def mutant_methods_modify_content_type(headers, url, method, data, files):
     logger.info(TAG + "==>mutant_methods_modify_content_type")
     logger.debug(TAG + "==>headers: " + str(headers))
+    mutant_payloads = []
     if 'Content-Type' in headers:
-        headers['Content-Type'] += ';application/json'
-    return headers,url,method,data,files
+        for content_type in content_types:
+            headers['Content-Type'] += ';' + content_type
+            mutant_payloads.append({
+                'headers': headers,
+                'url': url,
+                'method': method,
+                'data': data,
+                'files': files
+            })
+    return mutant_payloads
 
 
+# change content-type to dict of content-type
+def mutant_methods_change_content_type(headers, url, method, data, files):
+    logger.info(TAG + "==>mutant_methods_modify_content_type")
+    logger.debug(TAG + "==>headers: " + str(headers))
+    mutant_payloads = []
+    if 'Content-Type' in headers:
+        for content_type in content_types:
+            headers['Content-Type'] = content_type + ';'
+            mutant_payloads.append({
+                'headers': headers,
+                'url': url,
+                'method': method,
+                'data': data,
+                'files': files
+            })
 
-mutant_methods = [mutant_methods_modify_content_type]
+    return mutant_payloads
 
 
-def prowler_begin_to_mutant_payloads(headers, url, method, data,files):
+mutant_methods = [mutant_methods_modify_content_type, mutant_methods_change_content_type]
+
+
+def prowler_begin_to_mutant_payloads(headers, url, method, data, files):
     logger.info(TAG + "==>begin to mutant payloads")
     mutant_payloads = []
     for mutant_method in mutant_methods:
         logger.info(TAG + "==>mutant method: " + str(mutant_method))
-        headers,url,method,data,files = mutant_method(headers,url,method,data,files)
-        mutant_payloads.append({
-            'headers': headers,
-            'url': url,
-            'method': method,
-            'data': data,
-            'files': files
-        })
-    return mutant_payloads  
-
+        sub_mutant_payloads = mutant_method(headers, url, method, data, files)
+        mutant_payloads.extend(sub_mutant_payloads)
+    return mutant_payloads
