c162c9543ef10b393123c8cfb1bda04639e0c471
stcirclear <1433481694@qq.com>
2024-08-27 23:40:58 +0800

    add content-types dict

---
+++ utils/dictUtils.py
@@ -0,0 +1,18 @@
+content_types = [
+    "application/json",
+    "application/xml",
+    "application/x-www-form-urlencoded",
+    "multipart/form-data",
+    "text/plain",
+    "text/html",
+    "text/css",
+    "text/javascript",
+    "text/csv",
+    "text/xml",
+    "text/plain",
+    "text/html",
+    "text/css",
+    "text/javascript",
+    "text/csv",
+    "text/xml",
+]
